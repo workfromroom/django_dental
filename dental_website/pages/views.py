22de7de41e134ad69ce774bf766675dafe6109c2
toyeex <toyeex@gmail.com>
2020-09-28 15:49:25 +0700

    Sampai Video 23

--- dental_website/pages/views.py
+++ dental_website/pages/views.py
@@ -9,7 +9,16 @@ def blog(request):
 def blog_details(request):
     return render(request, 'blog-details.html',{})
 def contact(request):
-    return render(request, 'contact.html',{})
+    if request.method == "POST":
+        message_name = request.POST['message_name']
+        message_email = request.POST['message_email']
+        message = request.POST['message']
+
+        send_mail(
+            message_name,message,message_email,['toyeex@gmail.com'],fail_silently=False,
+        )
+    else:
+        return render(request, 'contact.html',{})
 def pricing(request):
     return render(request, 'pricing.html',{})
 def service(request):
